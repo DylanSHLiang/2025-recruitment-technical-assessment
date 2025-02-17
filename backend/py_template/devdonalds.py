ea45b96922b2e6d2d5ad4745caa8d0459a5f37ff
Dylan Liang <z5480575@ad.unsw.edu.au>
2025-02-17 12:47:04 +1100

    draft implementation

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -28,7 +28,7 @@ class Ingredient(CookbookEntry):
 app = Flask(__name__)
 
 # Store your recipes here!
-cookbook = None
+cookbook = {}
 
 # Task 1 helper (don't touch)
 @app.route("/parse", methods=['POST'])
@@ -43,7 +43,12 @@ def parse():
 # [TASK 1] ====================================================================
 # Takes in a recipeName and returns it in a form that 
 def parse_handwriting(recipeName: str) -> Union[str | None]:
-	# TODO: implement me
+	recipeName = re.sub('[-_]', ' ', recipeName)
+	recipeName = re.sub(' +', ' ', recipeName)
+	recipeName = re.sub('[^a-zA-Z ]', '', recipeName)
+	recipeName = ' '.join(word.capitalize() for word in recipeName.split())
+	if len(recipeName) <= 0:
+		return None
 	return recipeName
 
 
@@ -51,16 +56,86 @@ def parse_handwriting(recipeName: str) -> Union[str | None]:
 # Endpoint that adds a CookbookEntry to your magical cookbook
 @app.route('/entry', methods=['POST'])
 def create_entry():
-	# TODO: implement me
-	return 'not implemented', 500
+	data = request.get_json()
+	recipe_name = data['name']
+	recipe_type = data['type']
+
+	if recipe_name in cookbook:
+		return 'entry name is not unique', 400
+
+	if recipe_type == 'recipe':
+		seen = set()
+
+		for item in data['requiredItems']:
+			if item['name'] in seen:
+				return 'requiredItems can only have one element per name', 400
+			seen.add(item['name'])
+        
+		cookbook[recipe_name] = {
+            'type': 'recipe',
+            'name': recipe_name,
+            'requiredItems': data['requiredItems']
+        }
+	elif recipe_type == 'ingredient':
+		if data['cookTime'] < 0:
+			return 'cookTime must be greater or equal to 0', 400
+	
+		cookbook[recipe_name] = {
+            'type': 'ingredient',
+            'name': recipe_name,
+            'cookTime': data['cookTime']
+        }
+	else:
+		return 'entry type must be either "recipe" or "ingredient"', 400
+
+	return '', 200
 
 
 # [TASK 3] ====================================================================
+# Helper function to recursively get ingredients
+def get_ingredients(ingredients, required_items, mult):
+	for item in required_items:
+		if item['name'] not in cookbook:
+			return 'ingredient not in cookbook', 400
+		
+		if cookbook[item['name']]['type'] == 'recipe':
+			msg, status = get_ingredients(ingredients, cookbook[item['name']]['requiredItems'], mult * item['quantity'])
+			if status != 200:
+				return msg, status
+		else:
+			ingredients[item['name']] = ingredients.get(item['name'], 0) + item['quantity'] * mult
+
+	return None, 200
+
 # Endpoint that returns a summary of a recipe that corresponds to a query name
 @app.route('/summary', methods=['GET'])
 def summary():
-	# TODO: implement me
-	return 'not implemented', 500
+	recipe_name = request.args.get('name')
+
+	if recipe_name not in cookbook:
+		return 'recipe not found', 400
+	if cookbook[recipe_name]['type'] != 'recipe':
+		return 'name is not a recipe name', 400
+	
+	recipe =  {
+		'type': 'recipe',
+		'name': recipe_name,
+	}
+
+	ingredients = {}
+	msg, status = get_ingredients(ingredients, cookbook[recipe_name]['requiredItems'], 1)
+	print(ingredients)
+	if status != 200:
+		return msg, status
+	recipe['ingredients'] = [
+		{
+			'name': name,
+			'quantity': quantity
+		}
+		for name, quantity in ingredients.items()
+	]
+
+	return recipe, 200
 
 
 # =============================================================================
